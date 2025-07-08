a2834009caf7bee0659730b04730f95a0c55c921
Tarun Kaushik <tarunkaushik83969@gmail.com>
2025-07-08 11:14:25 +0530

    I have Covered Encapsulation by using getter and setter functions and properties

---
+++ CSharpJourney/Encapsulation/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpJourney.Encapsulation
+{
+    public class BankAccount
+    {
+        private string accountNumber;
+        private decimal balance;
+
+        public BankAccount(string accountNumber, decimal balance)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+        }
+
+         public void Deposit(decimal amount)
+        {
+            if(amount > 0)
+            {
+                balance += amount;
+                Console.Write("Succeed ! \t");
+                Console.Write($"Your Account has been credited with {amount}\t");
+                Console.WriteLine($"Now your updated balance is {balance}");
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid Amount !");
+            }         
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if(amount <= balance)
+            {
+                balance -= amount;
+                Console.Write("Succeed ! \t");
+                Console.Write($"{amount} has been deducted from your account \t");
+                Console.WriteLine($"Updated balance is {balance}");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance !");
+            }
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"Your balance is ${balance}");
+            Console.WriteLine($"Your Account Number is ${accountNumber}");
+        }
+
+
+        public static void Run()
+        {
+            BankAccount b1 = new BankAccount("HDFC131021", 5000);
+            b1.display();
+            b1.Deposit(5000m);
+            b1.Withdraw(2000m);
+            b1.display();
+
+
+        }
+
+    }
+}
---
+++ CSharpJourney/Encapsulation/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpJourney.Encapsulation
+{
+    public class Student
+    {
+        public int RollNumber { get; }
+
+        public string Name { get; set; }
+
+        private int marks;
+
+        public int Marks
+        {
+            get
+            {
+                return marks;
+            }
+            set
+            {
+                if(value>0 && value <= 100)
+                {
+                    marks = value;
+                }
+            }
+        }
+
+        public Student(int rollNumber, string name, int marks)
+        {
+            RollNumber = rollNumber;
+            Name = name;
+            Marks = marks;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Roll Number = {RollNumber}");
+            Console.WriteLine($"Name = {Name}");
+            Console.WriteLine($"Marks = {Marks}");
+        }
+
+        public static void Run()
+        {
+            Student s1 = new Student(2821109, "Tarun Kaushik", 98);
+            s1.Display();
+
+        }
+    }
+}
